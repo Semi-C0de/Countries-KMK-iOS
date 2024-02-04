db9dbd3080c38f445b1ee7b37d8ba5a6415484e4
Semi-C0de <lalithg@outlook.com>
2024-02-03 16:14:18 -0800

    Initial Commit

---
+++ Countries KMK/Countries_KMKApp.swift
@@ -0,0 +1,17 @@
+//
+//  Countries_KMKApp.swift
+//  Countries KMK
+//
+//  Created by Lalith Shravan Guruprasad on 2/3/24.
+//
+
+import SwiftUI
+
+@main
+struct Countries_KMKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
