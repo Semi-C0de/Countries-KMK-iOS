51f45ea2fcbcf7510d36081fc9fb16bfc89348d6
Semi-C0de <lalithg@outlook.com>
2024-02-03 23:04:11 -0800

    More UI changes.

--- Countries KMK/ContentView.swift
+++ Countries KMK/ContentView.swift
@@ -18,25 +18,41 @@ struct ContentView: View {
                 
                 VStack(alignment: .center) {
                     
-                    Text("USA 🇺🇸")
-                        .font(.system(size: 25))
-                        .frame(width: 350, height: 100)
-                        .background(.blue)
+                    CountryButton(name: "USA 🇺🇸", colors: [Color.red, Color.blue])
+                        .padding(.bottom, 5.0)
                     
-                    Text("USA 🇺🇸")
-                        .font(.system(size: 25))
-                        .frame(width: 350, height: 100)
-                        .background(.blue)
+                    CountryButton(name: "USA 🇺🇸", colors: [Color.red, Color.blue])
+                        .padding(.bottom, 5.0)
                     
-                    Text("USA 🇺🇸")
-                        .font(.system(size: 25))
-                        .frame(width: 350, height: 100)
-                        .background(.blue)
+                    CountryButton(name: "USA 🇺🇸", colors: [Color.red, Color.blue])
                     
                 }
                 .padding()
             }
         }
+    }
+}
+
+struct CountryButton:View {
+    
+    let name:String
+    let colors:[Color]!
+    
+    var bgPattern: any ShapeStyle {
+        if let _ = self.colors {
+            return LinearGradient(gradient: Gradient(colors: colors), startPoint: .topLeading, endPoint: .bottomTrailing)
+        }
+        
+        return Color.blue
+    }
+    
+    var body: some View {
+        Text("USA 🇺🇸")
+            .font(.system(size: 25))
+            .frame(width: 350, height: 105)
+            .background(AnyShapeStyle(self.bgPattern))
+            .clipShape(RoundedRectangle(cornerRadius: 20.0))
+        
     }
 }
 
